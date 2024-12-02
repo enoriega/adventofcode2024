f4ccbf208e91018a6756c248efdd3511fdcde572
Enrique Noriega <enoriega@arizona.edu>
2024-12-02 15:45:11 -0700

    Added part 2 of day 1

--- day1.py
+++ day1.py
@@ -1,11 +1,14 @@
 # https://adventofcode.com/2024/day/1
 
 import typer
-import heapq
+import logging
 from pathlib import Path
 from tqdm import tqdm
 
-def main(input_file:Path):
+app = typer.Typer()
+
+def read_inputs(input_file:Path) -> tuple[list[int], list[int]]:
+	""" Reads the input file and returns the left and right lists in used for the challenge """
 	left, right = [], []
 	
 	with input_file.open() as f:
@@ -16,9 +19,16 @@ def main(input_file:Path):
 			left.append(l)
 			right.append(r)
 
-	print(f"Number of elements: {len(left), len(right)}")
+	logging.info(f"Number of elements: {len(left), len(right)}")
+	return left, right
 
+@app.command()
+def part1(input_file:Path) -> None:
+	""" Computes the total distance between the matching ranked pairs in the parallel lists """
+	
+	left, right = read_inputs(input_file)
 	# Heapify the lists
+	import heapq
 	heapq.heapify(left)
 	heapq.heapify(right)
 
@@ -28,18 +38,35 @@ def main(input_file:Path):
 	while left:
 		l = heapq.heappop(left)
 		r = heapq.heappop(right)
+		# Compute the distance between the pair
 		max_ = max(l, r)
 		min_ = min(l, r)
 		d = max_ - min_
+		# Accumulate it
 		distance += d
 		progress.update(1)
 	progress.close()
 
 	print(f'Total distance: {distance}')
 
+@app.command()
+def part2(input_path:Path) -> None:
+	""" Computes the similarity score for part 2 """
+	left, right = read_inputs(input_path)
+
+	from collections import Counter
+	right = Counter(right)
+
+	score = 0
+	for l in tqdm(left, desc="Computing similarity", unit="numbers"):
+		times = right[l]
+		score += l*times
+
+	print(f"Similarity: {score}")
+
 
 if __name__ == "__main__":
-	typer.run(main)
+	app()
 
 
 
